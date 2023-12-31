aa41899b4759e06f5d0ccbaaa9ef6a58eedba2d7
f0rest775 <f0rest15@hotmail.com>
2023-12-30 23:08:08 -0300

    commit changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,135 @@
-import Image from 'next/image'
+"use client";
+import { Oleo_Script } from "next/font/google";
+import { BINGO } from "@/utils/bingo";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+
+const oswald = Oleo_Script({
+  weight: "700",
+  subsets: ["latin"],
+});
 
 export default function Home() {
+  const [bingo, setBingo] = useState(BINGO);
+  const [numberActualy, setnumberActualy] = useState("00");
+
+  async function sortBingo() {
+    const notChosen = bingo.filter((n) => !n.status);
+    if (notChosen.length === 0) {
+      alert("Todos os números já foram escolhidos!");
+      return;
+    }
+    const i = Math.floor(Math.random() * notChosen.length);
+    const numberChonsen = notChosen[i];
+    const newB = bingo.map((n) =>
+      n.id === numberChonsen.id ? { ...n, status: true } : n
+    );
+    setBingo(newB);
+  }
+
+  function resetBingo() {
+    const newB = bingo.map((n) => ({ ...n, status: false }));
+    setBingo(newB);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="min-h-screen w-full flex justify-center flex-col pb-10 mx-auto px-4 2xl:px-16 space-y-4 bg-neutral-100 select-none">
+      <div className="flex items-center justify-around pt-4 gap-8">
+        <div className="flex flex-col gap-4">
+          <Button
+            onClick={sortBingo}
+            variant={"outline"}
+            className={cn(
+              "hover:bg-[#B68133] border-[#B68133] hover:text-white font-semibold text-[#b68133]"
+            )}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Girar o BINGO!!
+          </Button>
+
+          <Button
+            onClick={resetBingo}
+            variant={"outline"}
+            className={cn(
+              "hover:bg-[#B68133] border-[#B68133] hover:text-white font-semibold text-[#b68133]"
+            )}
+          >
+            Nova rodada!!
+          </Button>
+        </div>
+
+        <div className="flex items-center justify-center gap-8">
+          <div
+            className={`${oswald.className} text-7xl font-semibold text-[#B68132]`}
+          >
+            Bingo Família Coco
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <div className={`${oswald.className} text-4xl text-[#934C14]`}>
+              Feliz
+            </div>
+            <div
+              className={`${oswald.className} text-[#B68132] text-8xl font-black text pt-2`}
+            >
+              2024
+            </div>
+          </div>
+        </div>
+
+        <Card
+          className={cn(
+            "bg-[#B68133] shadow-[0_20px_50px_rgba(145,_145,_145,_0.7)] w-52 h-52 rounded-full flex items-center justify-center"
+          )}
+        >
+          <h2 className="text-8xl font-bold text-white">{numberActualy}</h2>
+        </Card>
+      </div>
+      <div className="grid grid-cols-16 gap-4 p-4 bg-white shadow-[0_20px_50px_rgba(182,_129,_50,_0.7)] rounded-md">
+        <div className="col-span-1 space-y-4">
+          <div
+            className={`${oswald.className} bg-[#B68132] shadow-lg w-full text-neutral-100 text-7xl px-3 2xl:py-2 font-bold rounded-md flex items-center justify-center`}
+          >
+            <h2>B</h2>
+          </div>
+          <div
+            className={`${oswald.className} bg-[#B68132] shadow-lg w-full text-neutral-100 text-7xl px-3 2xl:py-2 font-bold rounded-md flex items-center justify-center`}
+          >
+            <h2>I</h2>
+          </div>
+          <div
+            className={`${oswald.className} bg-[#B68132] shadow-lg w-full text-neutral-100 text-7xl px-3 2xl:py-2 font-bold rounded-md flex items-center justify-center`}
+          >
+            <h2>N</h2>
+          </div>
+          <div
+            className={`${oswald.className} bg-[#B68132] shadow-lg w-full text-neutral-100 text-7xl px-3 2xl:py-2 font-bold rounded-md flex items-center justify-center`}
+          >
+            <h2>G</h2>
+          </div>
+          <div
+            className={`${oswald.className} bg-[#B68132] shadow-lg w-full text-neutral-100 text-7xl px-3 2xl:py-2 font-bold rounded-md flex items-center justify-center`}
+          >
+            <h2>O</h2>
+          </div>
+        </div>
+        <div className="col-span-15 grid grid-cols-15 gap-4">
+          {bingo.map((B, index) => (
+            <div
+              key={index}
+              className={cn(
+                "shadow-2xl shadow-neutral-400 w-full text-6xl max-2xl:text-5xl px-3 py-2 font-bold rounded-md flex items-center justify-center",
+                B.status
+                  ? "bg-[#B68132] text-neutral-100 animate-f0restt"
+                  : "bg-neutral-100 text-[#B68132]"
+              )}
+            >
+              <h2>{B.id}</h2>
+            </div>
+          ))}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
---
+++ src/utils/bingo.ts
@@ -0,0 +1,314 @@
+const BINGO = [
+  {
+    id: "1",
+    status: false,
+  },
+  {
+    id: "2",
+    status: false,
+  },
+  {
+    id: "3",
+    status: false,
+  },
+  {
+    id: "4",
+    status: false,
+  },
+  {
+    id: "5",
+    status: false,
+  },
+  {
+    id: "6",
+    status: false,
+  },
+
+  {
+    id: "7",
+    status: false,
+  },
+  {
+    id: "8",
+    status: false,
+  },
+
+  {
+    id: "9",
+    status: false,
+  },
+  {
+    id: "10",
+    status: false,
+  },
+  {
+    id: "11",
+    status: false,
+  },
+
+  {
+    id: "12",
+    status: false,
+  },
+
+  {
+    id: "13",
+    status: false,
+  },
+  {
+    id: "14",
+    status: false,
+  },
+  {
+    id: "15",
+    status: false,
+  },
+
+  {
+    id: "16",
+    status: false,
+  },
+  {
+    id: "17",
+    status: false,
+  },
+
+  {
+    id: "18",
+    status: false,
+  },
+  {
+    id: "19",
+    status: false,
+  },
+  {
+    id: "20",
+    status: false,
+  },
+
+  {
+    id: "21",
+    status: false,
+  },
+  {
+    id: "22",
+    status: false,
+  },
+
+  {
+    id: "23",
+    status: false,
+  },
+  {
+    id: "24",
+    status: false,
+  },
+  {
+    id: "25",
+    status: false,
+  },
+  {
+    id: "26",
+    status: false,
+  },
+  {
+    id: "27",
+    status: false,
+  },
+  {
+    id: "28",
+    status: false,
+  },
+  {
+    id: "29",
+    status: false,
+  },
+  {
+    id: "30",
+    status: false,
+  },
+  {
+    id: "31",
+    status: false,
+  },
+  {
+    id: "32",
+    status: false,
+  },
+  {
+    id: "33",
+    status: false,
+  },
+  {
+    id: "34",
+    status: false,
+  },
+  {
+    id: "35",
+    status: false,
+  },
+  {
+    id: "36",
+    status: false,
+  },
+  {
+    id: "37",
+    status: false,
+  },
+  {
+    id: "38",
+    status: false,
+  },
+  {
+    id: "39",
+    status: false,
+  },
+  {
+    id: "40",
+    status: false,
+  },
+  {
+    id: "41",
+    status: false,
+  },
+  {
+    id: "42",
+    status: false,
+  },
+  {
+    id: "43",
+    status: false,
+  },
+  {
+    id: "44",
+    status: false,
+  },
+  {
+    id: "45",
+    status: false,
+  },
+  {
+    id: "46",
+    status: false,
+  },
+  {
+    id: "47",
+    status: false,
+  },
+  {
+    id: "48",
+    status: false,
+  },
+  {
+    id: "49",
+    status: false,
+  },
+  {
+    id: "50",
+    status: false,
+  },
+  {
+    id: "51",
+    status: false,
+  },
+  {
+    id: "52",
+    status: false,
+  },
+  {
+    id: "53",
+    status: false,
+  },
+
+  {
+    id: "54",
+    status: false,
+  },
+
+  {
+    id: "55",
+    status: false,
+  },
+  {
+    id: "56",
+    status: false,
+  },
+  {
+    id: "57",
+    status: false,
+  },
+  {
+    id: "58",
+    status: false,
+  },
+  {
+    id: "59",
+    status: false,
+  },
+  {
+    id: "60",
+    status: false,
+  },
+  {
+    id: "61",
+    status: false,
+  },
+  {
+    id: "62",
+    status: false,
+  },
+  {
+    id: "63",
+    status: false,
+  },
+  {
+    id: "64",
+    status: false,
+  },
+  {
+    id: "65",
+    status: false,
+  },
+  {
+    id: "66",
+    status: false,
+  },
+  {
+    id: "67",
+    status: false,
+  },
+  {
+    id: "68",
+    status: false,
+  },
+  {
+    id: "69",
+    status: false,
+  },
+  {
+    id: "70",
+    status: false,
+  },
+  {
+    id: "71",
+    status: false,
+  },
+  {
+    id: "72",
+    status: false,
+  },
+  {
+    id: "73",
+    status: false,
+  },
+  {
+    id: "74",
+    status: false,
+  },
+  {
+    id: "75",
+    status: false,
+  },
+];
+
+export { BINGO };
